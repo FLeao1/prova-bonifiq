124449582b7c766fd8e312d8c6c1be7a5ec81e95
Pedro Ribeiro <pribeiro@provertec.com.br>
2023-04-24 05:28:02 -0300

    ajuste

--- Controllers/Parte2Controller.cs
+++ Controllers/Parte2Controller.cs
@@ -10,32 +10,153 @@ namespace ProvaPub.Controllers
 	[Route("[controller]")]
 	public class Parte2Controller :  ControllerBase
 	{
-		/// <summary>
-		/// Precisamos fazer algumas alterações:
-		/// 1 - Não importa qual page é informada, sempre são retornados os mesmos resultados. Faça a correção.
-		/// 2 - Altere os códigos abaixo para evitar o uso de "new", como em "new ProductService()". Utilize a Injeção de Dependência para resolver esse problema
-		/// 3 - Dê uma olhada nos arquivos /Models/CustomerList e /Models/ProductList. Veja que há uma estrutura que se repete. 
-		/// Como você faria pra criar uma estrutura melhor, com menos repetição de código? E quanto ao CustomerService/ProductService. Você acha que seria possível evitar a repetição de código?
-		/// 
-		/// </summary>
-		TestDbContext _ctx;
-		public Parte2Controller(TestDbContext ctx)
-		{
-			_ctx = ctx;
-		}
-	
-		[HttpGet("products")]
-		public ProductList ListProducts(int page)
-		{
-			var productService = new ProductService(_ctx);
-			return productService.ListProducts(page);
-		}
+        /// <summary>
+        /// Precisamos fazer algumas alterações:
+        /// 1 - Não importa qual page é informada, sempre são retornados os mesmos resultados. Faça a correção.
+        /// 2 - Altere os códigos abaixo para evitar o uso de "new", como em "new ProductService()". Utilize a Injeção de Dependência para resolver esse problema
+        /// 3 - Dê uma olhada nos arquivos /Models/CustomerList e /Models/ProductList. Veja que há uma estrutura que se repete. 
+        /// Como você faria pra criar uma estrutura melhor, com menos repetição de código? E quanto ao CustomerService/ProductService. Você acha que seria possível evitar a repetição de código?
+        /// 
+        /// </summary>
 
-		[HttpGet("customers")]
-		public CustomerList ListCustomers(int page)
-		{
-			var customerService = new CustomerService(_ctx);
-			return customerService.ListCustomers(page);
-		}
-	}
+        //public abstract class BaseList<T>
+        //{
+        //    public int Page { get; set; }
+        //    public int PageSize { get; set; }
+        //    public int TotalPages { get; set; }
+        //    public int TotalItems { get; set; }
+        //    public List<T> Items { get; set; }
+
+        //    protected BaseList(int page, int pageSize, int totalPages, int totalItems, List<T> items)
+        //    {
+        //        Page = page;
+        //        PageSize = pageSize;
+        //        TotalPages = totalPages;
+        //        TotalItems = totalItems;
+        //        Items = items;
+        //    }
+        //}
+
+        //public class CustomerList : BaseList<Customer>
+        //{
+        //    public CustomerList(int page, int pageSize, int totalPages, int totalItems, List<Customer> items)
+        //        : base(page, pageSize, totalPages, totalItems, items)
+        //    {
+        //    }
+        //}
+
+        //public class ProductList : BaseList<Product>
+        //{
+        //    public ProductList(int page, int pageSize, int totalPages, int totalItems, List<Product> items)
+        //        : base(page, pageSize, totalPages, totalItems, items)
+        //    {
+        //    }
+        //}
+
+        //public interface IBaseService<T>
+        //{
+        //    BaseList<T> List(int page);
+        //}
+
+        //public class CustomerService : IBaseService<Customer>
+        //{
+        //    private readonly TestDbContext _ctx;
+
+        //    public CustomerService(TestDbContext ctx)
+        //    {
+        //        _ctx = ctx;
+        //    }
+
+        //    public BaseList<Customer> List(int page)
+        //    {
+        //        var pageSize = 10;
+        //        var totalItems = _ctx.Customers.Count();
+        //        var totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+        //        var items = _ctx.Customers.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+        //        return new CustomerList(page, pageSize, totalPages, totalItems, items);
+        //    }
+        //}
+
+        //public class ProductService : IBaseService<Product>
+        //{
+        //    private readonly TestDbContext _ctx;
+
+        //    public ProductService(TestDbContext ctx)
+        //    {
+        //        _ctx = ctx;
+        //    }
+
+        //    public BaseList<Product> List(int page)
+        //    {
+        //        var pageSize = 10;
+        //        var totalItems = _ctx.Products.Count();
+        //        var totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+        //        var items = _ctx.Products.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+        //        return new ProductList(page, pageSize, totalPages, totalItems, items);
+        //    }
+        //}
+
+        //public class Parte2ControllerImpl : Controller
+        //{
+        //    private readonly IBaseService<Customer> _customerService;
+        //    private readonly IBaseService<Product> _productService;
+        //    TestDbContext _ctx;
+
+        //    public Parte2ControllerImpl(IBaseService<Customer> customerService, IBaseService<Product> productService)
+        //    {
+        //        _customerService = customerService;
+        //        _productService = productService;
+        //    }
+
+        //    //[HttpGet("products")]
+        //    //public ProductList ListProducts(int page)
+        //    //{
+        //    //	var productService = new ProductService(_ctx);
+        //    //	return productService.ListProducts(page);
+        //    //}
+
+        //    [HttpGet("products")]
+        //    public ProductList ListProducts(int page)
+        //    {
+        //        var productService = new ProductService(_ctx);
+        //        return _productService.List(page) as ProductList;
+        //    }
+
+        //    //[HttpGet("customers")]
+        //    //public CustomerList ListCustomers(int page)
+        //    //{
+        //    //    return _customerService.List(page) as CustomerList;
+        //    //}
+
+        //    [HttpGet("customers")]
+        //    public CustomerList ListCustomers(int page)
+        //    {
+        //        var customerService = new CustomerService(_ctx);
+        //        return customerService.List(page) as CustomerList;
+        //    }
+
+        //}
+
+        TestDbContext _ctx;
+        public Parte2Controller(TestDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet("products")]
+        public ProductList ListProducts(int page)
+        {
+            var productService = new ProductService(_ctx);
+            return productService.ListProducts(page);
+        }
+
+        [HttpGet("customers")]
+        public CustomerList ListCustomers(int page)
+        {
+            var customerService = new CustomerService(_ctx);
+            return customerService.ListCustomers(page);
+        }
+    }
 }
--- Controllers/Parte3Controller.cs
+++ Controllers/Parte3Controller.cs
@@ -5,21 +5,82 @@ using ProvaPub.Services;
 
 namespace ProvaPub.Controllers
 {
-	
-	/// <summary>
-	/// Esse teste simula um pagamento de uma compra.
-	/// O método PayOrder aceita diversas formas de pagamento. Dentro desse método é feita uma estrutura de diversos "if" para cada um deles.
-	/// Sabemos, no entanto, que esse formato não é adequado, em especial para futuras inclusões de formas de pagamento.
-	/// Como você reestruturaria o método PayOrder para que ele ficasse mais aderente com as boas práticas de arquitetura de sistemas?
-	/// </summary>
-	[ApiController]
-	[Route("[controller]")]
-	public class Parte3Controller :  ControllerBase
-	{
-		[HttpGet("orders")]
-		public async Task<Order> PlaceOrder(string paymentMethod, decimal paymentValue, int customerId)
-		{
-			return await new OrderService().PayOrder(paymentMethod, paymentValue, customerId);
-		}
-	}
+
+    /// <summary>
+    //// Esse teste simula um pagamento de uma compra.
+    //// O método PayOrder aceita diversas formas de pagamento. Dentro desse método é feita uma estrutura de diversos "if" para cada um deles.
+    //// Ssabemos, no entanto, que esse formato não é adequado, em especial para futuras inclusões de formas de pagamento.
+    //// Como você reestruturaria o método PayOrder para que ele ficasse mais aderente com as boas práticas de arquitetura de sistemas?
+
+    [ApiController]
+    [Route("[controller]")]
+    public class Parte3Controller : ControllerBase
+    {
+        ////	[HttpGet("orders")]
+        ////	public async Task<Order> PlaceOrder(string paymentMethod, decimal paymentValue, int customerId)
+        ////	{
+        ////		return await new OrderService().PayOrder(paymentMethod, paymentValue, customerId);
+        ////	}
+        ////}
+
+        public interface IPaymentStrategy
+        {
+            bool Pay(decimal paymentValue, int customerId);
+        }
+
+        public class PixPaymentStrategy : IPaymentStrategy
+        {
+            public bool Pay(decimal paymentValue, int customerId)
+            {
+                //Faz o pagamento com Pix
+                return true;
+            }
+        }
+
+        public class CreditCardPaymentStrategy : IPaymentStrategy
+        {
+            public bool Pay(decimal paymentValue, int customerId)
+            {
+                //Faz o pagamento com cartão de crédito
+                return true;
+            }
+        }
+
+        public class PayPalPaymentStrategy : IPaymentStrategy
+        {
+            public bool Pay(decimal paymentValue, int customerId)
+            {
+                //Faz o pagamento com PayPal
+                return true;
+            }
+        }
+
+        public class OrderService
+        {
+            private readonly Dictionary<string, IPaymentStrategy> _paymentStrategies = new Dictionary<string, IPaymentStrategy>()
+    {
+        {"pix", new PixPaymentStrategy()},
+        {"creditcard", new CreditCardPaymentStrategy()},
+        {"paypal", new PayPalPaymentStrategy()}
+    };
+
+            public async Task<Order> PayOrder(string paymentMethod, decimal paymentValue, int customerId)
+            {
+                if (_paymentStrategies.TryGetValue(paymentMethod, out var paymentStrategy))
+                {
+                    if (paymentStrategy.Pay(paymentValue, customerId))
+                    {
+                        //Faz o pagamento
+                        return new Order()
+                        {
+                            Value = paymentValue
+                        };
+                    }
+                }
+
+                throw new ArgumentException("Forma de pagamento inválida.");
+            }
+        }
+
+    }
 }
